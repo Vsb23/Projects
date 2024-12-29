f68643b9d62b675f1fa89b069862a25b1da96075
Vito Stefano Birardi <v.birardi3@studenti.uniba.it>
2024-12-29 23:03:02 +0100

    Aggiunta eliminazione
    Adesso ogni volta che viene riprodotto il file, alla sua chiusura viene eliminato

--- Scripts/Anime random player/alp.py
+++ Scripts/Anime random player/alp.py
@@ -3,15 +3,16 @@ import psutil
 import random
 animes = os.listdir('series')
 
+mpv = [item.pid for item in psutil.process_iter() if item.name() == 'mpv.exe']
 
 
-mpv = [item.pid for item in psutil.process_iter() if item.name() == 'mpv']
-print(mpv)
 
 with os.scandir('series') as series:
     anime = random.choice(list(series))
     episode = os.listdir(anime.path)
     print(f'Now watching: {anime.name} episode: {episode[0]}')
     os.system(f'mpv series/"{anime.name}/{episode[0]}"')
-
+    while mpv == []:
+        os.remove(f"series/{anime.name}/{episode[0]}")
+        print(f"Eliminato il file {episode[0]} di {anime.name}")
 
